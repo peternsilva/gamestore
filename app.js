d846b0e5c7043cdec85641703b888592dc0a06f4
Pete Silva <peter.silva@galvanize.com>
2018-06-28 15:15:28 -0700

    users migration modified, basic user registration working

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bcrypt = require('bcrypt')
+var knex = require('./knex')
+var jwt = require('jsonwebtoken')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -25,6 +28,66 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 // app.use('/products', productsRouter);
 // app.use('/orders', ordersRouter);
+
+// REGISTER a user
+app.post('/register', (req, res, next) => {
+  console.log('/register was hit');
+  
+  // check the email doesn't already exist in users table
+  knex('users')
+  .where('email', req.body.email)
+  .first()
+  .then((user) => {
+    if(user) {
+      throw new Error('Oops that email is already used')
+    }
+    
+    // hash the password
+    var hashed = bcrypt.hashSync(req.body.password, 8)
+    
+    // create new user record with the email + hashed password
+    knex('users')
+    .insert({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashed
+    })
+    .returning('*')
+    .then((result) => {
+      res.redirect('/login')
+    })
+  })
+  .catch((err) => {
+    next(err)
+  })
+})
+
+// LOGIN the user
+app.post('/login', (req, res, next) => {
+  // take in email, password
+  // find the user with this email in the db
+  knex('users')
+  .where('email', req.body.email)
+  .first()
+  .then((user) => {
+    if(user) {
+      // use bcrypt.compare to compare to the hashed password in the db
+      var passwordGood = bcrypt.compareSync(req.body.password, user.password)
+      // if all is good, create the token, and attach it as a cookie to the response
+      if(passwordGood) {
+        // create token
+        
+      }
+    } else {
+      throw new Error('Could not find that user')
+    }
+  })
+  .catch((err) => {
+    next(err)
+  })
+  
+  
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- migrations/20180619114825_users.js
+++ migrations/20180619114825_users.js
@@ -1,8 +1,9 @@
-
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', (table) => {
     table.increments()
     table.string('name').notNullable()
+    table.string('email').notNullable()
+    table.text('password').notNullable()
     table.timestamps(true, true)
   })
 }
